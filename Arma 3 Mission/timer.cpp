3b16ceffd3058ca664d4e76c2c006122a5f08953
sqwishy trick <sqwishybon@gmail.com>
2013-10-03 08:04:21 -0700

    Adds a timer module for some janky benchmarking

--- Arma 3 Mission/timer.cpp
+++ Arma 3 Mission/timer.cpp
@@ -1,8 +1,21 @@
+
 #include <windows.h>
+
+#include <time.h>
+#include <stdio.h>
 
 extern "C" {
     __declspec(dllexport) void __stdcall RVExtension(char *output, int outputSize, const char *function); 
 };
 
+clock_t t = 0;
+
 void __stdcall RVExtension(char *output, int outputSize, const char *function) {
+    if (0 == t) {
+        t = clock();
+    } else {
+        t = clock() - t;
+        sprintf(output, "%f", ((float)t)/CLOCKS_PER_SEC);
+        t = 0;
+    }
 }
