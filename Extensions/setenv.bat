3d49a16aaf0b9a7a2e2931874a623813851548fa
sqwishy trick <sqwishybon@gmail.com>
2013-10-05 16:50:51 -0700

    Local environment build configuration stuff ...
    Adds a sample setenv.local.bat file
    setenv runs setenv.local.bat

--- Extensions/setenv.bat
+++ Extensions/setenv.bat
@@ -1,10 +1,19 @@
 @echo off
+
+if not exist "setenv.local.bat" (
+    echo You don't have a setenv.local.bat file, you need one.
+    echo There should be a setenv.local.bat.sample file and I will try to copy it to setenv.local.bat and then exit. You should probably look at it and make sure it's correct for your configuration.
+    copy "setenv.local.bat.sample" "setenv.local.bat"
+    goto :end
+)
+
 where cl
 if %errorlevel% == 0 (
     echo This environment appears to already be configured, exiting
     goto :end
 )
 
+call "setenv.local.bat"
 set PATH=%PATH%;%~p0\env\bin
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 
